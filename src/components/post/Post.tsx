ee49bf52f26133d1cc0640cb73dfa0f8cb77e6c7
ShuheiKurinami <zhoupinglibo252@gmail.com>
2023-12-15 09:38:41 +0900

    途中だけどダミーデータでの実装中

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,8 +1,35 @@
 import React from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import "./Post.css";
+import { Users } from "../../dummyData";
 
-export const Post = () => {
+type posts =
+  | {
+      id: number;
+      desc: string;
+      photo: string;
+      date: string;
+      userId: number;
+      like: number;
+      comment: number;
+    }
+  | {
+      id: number;
+      photo: string;
+      date: string;
+      userId: number;
+      like: number;
+      comment: number;
+      desc?: undefined;
+    };
+
+type PostProps = {
+  post: posts;
+};
+
+export const Post = ({ post }: PostProps) => {
+  const user = Users.filter((user) => user.id === 1);
+  console.log(user);
   return (
     <div className="post">
       <div className="postWrapper">
@@ -14,23 +41,25 @@ export const Post = () => {
               className="postProfileImg"
             />
             <span className="postUsername">shinCode</span>
-            <span className="postDate">5分前</span>
+            <span className="postDate">{post.date}</span>
           </div>
           <div className="postTopRight">
             <MoreVertIcon />
           </div>
         </div>
 
-        <span className="postText">SNSを自作中です。</span>
-        <img src="./assets/post/1.jpeg" alt="" className="postImg" />
+        <span className="postText">{post.desc}</span>
+        <img src={post.photo} alt="" className="postImg" />
       </div>
       <div className="postBottom">
         <div className="postBottomLeft">
           <img src="./assets/heart.png" alt="" className="likeIcon" />
-          <span className="postLikeCounter">５人がいいねを押しました。</span>
+          <span className="postLikeCounter">
+            {post.like}人がいいねを押しました。
+          </span>
         </div>
         <div className="postBottomRight">
-          <span className="post CommentText">４のコメント</span>
+          <span className="post CommentText">{post.comment}のコメント</span>
         </div>
       </div>
     </div>

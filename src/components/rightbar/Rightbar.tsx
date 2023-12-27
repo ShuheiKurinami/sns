8c043e0ab259408d2085228edd67ab82c3c2064e
ShuheiKurinami <zhoupinglibo252@gmail.com>
2023-12-27 17:20:50 +0900

    rightbarのhtml作成

--- src/components/rightbar/Rightbar.tsx
+++ src/components/rightbar/Rightbar.tsx
@@ -54,7 +54,60 @@ export const Rightbar = ({ profile }: profileProps) => {
   };
 
   const ProfileRightbar = () => {
-    return <>profileのrightbarです。</>;
+    return (
+      <>
+        <h4 className="rightbarTitle">ユーザー情報</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">出身：</span>
+            <span className="rightbarInfoKey">福岡</span>
+          </div>
+          <h4 className="rightbarTitle">あなたの友達</h4>
+          <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+              <img
+                src="assets/person/1.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Shin Code</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="assets/person/2.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Shin Code</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Shin Code</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="assets/person/4.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Shin Code</span>
+            </div>
+            <div className="rightbarFollowing">
+              <img
+                src="assets/person/5.jpeg"
+                alt=""
+                className="rightbarFollowingImg"
+              />
+              <span className="rightbarFollowingName">Shin Code</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   };
   return (
     <div className="rightbar">

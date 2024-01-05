5e057868e34bf9dd80e09c4911d4ccbdee11cd60
ShuheiKurinami <zhoupinglibo252@gmail.com>
2024-01-05 16:46:16 +0900

    login画面途中

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Home } from "./pages/home/home";
 import { Profile } from "./pages/profile/Profile";
+import Login from "./pages/login/login";
 
 function App() {
   // return <Home />;
-  return <Profile />;
+  // return <Profile />;
+  return <Login />;
 }
 
 export default App;

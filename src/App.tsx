abdf1fd8cc566b24563e3226b63911309ed12e1e
ShuheiKurinami <zhoupinglibo252@gmail.com>
2024-01-08 12:49:00 +0900

    登録画面作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Home } from "./pages/home/home";
 import { Profile } from "./pages/profile/Profile";
 import Login from "./pages/login/login";
+import Register from "./pages/register/Register";
 
 function App() {
   // return <Home />;
   // return <Profile />;
-  return <Login />;
+  // return <Login />;
+  return <Register />;
 }
 
 export default App;
